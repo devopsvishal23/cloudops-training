e87d562ed14680d33cee00e56a0cb45e875efb9c
Vishal Shinde <devops.vishal23@gmail.com>
2026-06-18 15:12:03 +0530

    week5: fix syntax error

--- cloudops-upskill/week4-docker/app/app.py
+++ cloudops-upskill/week4-docker/app/app.py
@@ -1,15 +1,25 @@
 from flask import Flask, jsonify
 import os
+import psycopg2
 
 app = Flask(__name__)
 
+def get_db():
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
+
 @app.route("/health")
-def health():
-    return jsonify({"status": "ok", "env": os.getenv("APP_ENV", "local")})
+def health()
+    try:
+        conn = get_db()
+        conn.close()
+        db_status = "ok"
+    except Exception as e:
+        db_status = str(e)
+    return jsonify({"status": "ok", "db": db_status, "env": os.getenv("APP_ENV", "local")})
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Vishal-18-June-2026 cloudops upskill — week 4"})
+    return jsonify({"message": "cloudops upskill — week 4"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
